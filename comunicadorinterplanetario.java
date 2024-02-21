0c640923c48fcec09048bac958086d2c9b35bf95
agonzolm@myuax.com <agonzolm@myuax.com>
2024-02-21 11:49:40 +0100

    .

--- comunicadorinterplanetario.java
+++ comunicadorinterplanetario.java
@@ -10,4 +10,23 @@ public class comunicadorinterplanetario {
         return conteo;
     }
 
+    public static String invertirMensaje(String mensaje) {
+        StringBuilder mensajeInvertido = new StringBuilder();
+        for (int i = mensaje.length() - 1; i >= 0; i--) {
+            mensajeInvertido.append(mensaje.charAt(i));
+        }
+        return mensajeInvertido.toString();
+    }
+
+    public static boolean esPalindromo(String mensaje) {
+        String mensajeSinEspacios = mensaje.replaceAll("\\s+", "").toLowerCase();
+        int longitud = mensajeSinEspacios.length();
+        for (int i = 0; i < longitud / 2; i++) {
+            if (mensajeSinEspacios.charAt(i) != mensajeSinEspacios.charAt(longitud - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
  
