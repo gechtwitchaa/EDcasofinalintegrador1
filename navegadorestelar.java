9bca5a1c098aaebee481b02913d9afc448efef8f
agonzolm@myuax.com <agonzolm@myuax.com>
2024-02-21 11:46:55 +0100

    .

---
+++ navegadorestelar.java
@@ -0,0 +1,12 @@
+import java.util.Arrays; // Importa la clase java.util.Arrays
+
+public class navegadorestelar {
+    private int[][] terreno;
+    private int[][] construcciones;
+
+    public navegadorestelar(int[][] terreno, int[][] construcciones) {
+        this.terreno = terreno;
+        this.construcciones = construcciones;
+    }
+
+  
